5d2896c16ba6c1a015a9b96010077b28323cf2bc
tracepillars <trace.pillars@e7solutions.com>
2014-08-03 23:58:31 -0400

    dsa

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,11 +1,45 @@
-paper.install(window);
+var user_name = prompt('User Name:');
+if (user_name === null) {
+    user_name = "User " + 100000 * Math.random().toPrecision(5);
+}
 
+var socket = io();
+
+socket.emit('user name', user_name);
+
+$('#chatButton').click(function () {
+    var msgSelector = $('#chatInput');
+    var msg = msgSelector.val().trim();
+    if (msg !== "") {
+        socket.emit('chat message', msg);
+    }
+    msgSelector.val('');
+    return false;
+});
+
+socket.on('chat message', function (msg) {
+    if ($.trim(msg) !== "") {
+        $('#chatDisplay').append($('<li></li>').text($.trim(msg)));
+    }
+
+});
+
+socket.on('user list', function (msg) {
+    var userList = $("#users");
+    userList.empty();
+    for (var i = 0; i < msg.length; i++) {
+        if ($.trim(msg[i]) !== "") {
+            userList.append($("<li></li>").text($.trim(msg[i])))
+        }
+    }
+});
+
+paper.install(window);
 window.onload = function () {
     paper.setup('drawingArea');
     var tool = new Tool();
     var brush = document.getElementById("brush");
     var path;
-    var points = [];
 
     tool.onMouseDown = function (event) {
         path = new Path();
