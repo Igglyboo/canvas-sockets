7bfa4f7020a7e938de945017296c79af8db7519f
tracepillars <trace.pillars@e7solutions.com>
2014-08-03 23:50:39 -0400

    initial

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.get('/', function(req, res){
+server.listen(8000, "0.0.0.0");
+
+app.use(express.static(__dirname + '/assets'));
+
+app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
+var user_list = [];
+
+io.on('connection', function (socket) {
     var current_user;
 
-    socket.on('user name', function(user_name){
+    socket.on('user name', function (user_name) {
         current_user = user_name;
-        io.emit('chat message', current_user + " has connected.");
+        user_list.push(user_name);
+        io.emit('user list', user_list);
+        io.emit('new user joined', current_user);
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
+        var index = user_list.indexOf(current_user);
+        if (index !== -1) {
+            user_list.splice(index, 1);
+        }
         io.emit('chat message', current_user + " has disconnected.");
+        io.emit('user list', user_list);
     });
 
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function (msg) {
         io.emit('chat message', current_user + ": " + msg);
     });
-});
 
-http.listen(8000, "0.0.0.0");
+    socket.on('path', function (path) {
+        socket.broadcast.emit('path', path);
+    });
+
+    socket.on('project', function (path) {
+        socket.broadcast.emit('project', path);
+    });
+});
--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,83 +1,39 @@
-function init(canvasId, brushId, eraserId, resetId, saveId, imgHolder) {
-    var canvas = document.getElementById(canvasId);
-    var ctx = canvas.getContext("2d");
+paper.install(window);
 
-    var brush = document.getElementById(brushId);
-    var eraser = document.getElementById(eraserId);
-    var reset = document.getElementById(resetId);
-    var save = document.getElementById(saveId);
+window.onload = function () {
+    paper.setup('drawingArea');
+    var tool = new Tool();
+    var brush = document.getElementById("brush");
+    var path;
+    var points = [];
 
-    var brushColor = brush.value;
-    var eraserColor = eraser.value;
-
-    var image = new Image();
-    document.getElementById(imgHolder).appendChild(image);
-
-    var prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0;
-
-    var flag = false,
-        dot_flag = false;
-
-    var draw = function(color){
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = color;
-        ctx.stroke();
-        ctx.closePath();
+    tool.onMouseDown = function (event) {
+        path = new Path();
+        path.strokeColor = brush.value;
+        path.add(event.point);
     };
 
-    var findxy = function(res, e){
-        var color = e.button === 2 ? eraserColor : brushColor;
-
-        if (res == 'down') {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX - canvas.offsetLeft;
-            currY = e.clientY - canvas.offsetTop;
-
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = color;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-
-        if (res == 'up' || res == "out") {
-            flag = false;
-        }
-
-        if (res == 'move') {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw(color);
-            }
-        }
+    tool.onMouseDrag = function (event) {
+        path.add(event.point);
     };
 
-    canvas.onmousemove = function(e){findxy('move', e)};
-    canvas.onmousedown = function(e){findxy('down', e)};
-    canvas.onmouseup = function(e){findxy('up', e)};
-    canvas.onmouseout = function(e){findxy('out', e)};
-    canvas.oncontextmenu = function(){return false;};
+    tool.onMouseUp = function (event) {
+        socket.emit('path', path.exportJSON({ asString: true, precision: 1 }))
+    };
 
-    brush.onchange = function(){brushColor = this.value};
-    eraser.onchange = function(){eraserColor = this.value};
-    reset.onclick = function(){ctx.save(); ctx.setTransform(1, 0, 0, 1, 0, 0);
-        ctx.clearRect(0, 0, canvas.width, canvas.height); ctx.restore();};
-    save.onclick = function(){image.src = canvas.toDataURL();};
-}
+    socket.on('path', function (path_json) {
+        socketPath = new Path();
+        socketPath.importJSON(path_json);
+        view.draw();
+    });
 
+    socket.on('project', function (project_json) {
+        project.importJSON(project_json);
+    });
 
-
+    socket.on('new user joined', function (new_user) {
+        $('#chatDisplay').append($('<li></li>').text($.trim(new_user + " has connected.")));
+        socket.emit('project', project.exportJSON({ asString: true, precision: 1 }));
+    })
+};
 
